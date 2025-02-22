fedea6abeb15d8c85a075136021c6056047a0426
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-02-22 11:13:02 +0530

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template>
- 
+
   <div>
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog class="relative z-50 lg:hidden" @close="closeSidebar">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
+        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
+          leave-to="opacity-0">
           <div class="fixed inset-0 bg-gray-900/80" />
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
+          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full" enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
+            leave-to="-translate-x-full">
             <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
-              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
                   <button type="button" class="-m-2.5 p-2.5" @click="closeSidebar">
                     <span class="sr-only">Close sidebar</span>
@@ -21,26 +27,34 @@
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
                 <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=white" alt="Your Company" />
+                  <img class="h-8 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=white"
+                    alt="Your Company" />
                 </div>
                 <nav class="flex flex-1 flex-col">
                   <ul role="list" class="flex flex-1 flex-col gap-y-7">
                     <li>
                       <ul role="list" class="-mx-2 space-y-1">
                         <li v-for="item in navigation" :key="item.name">
-                          <a href="#" @click.prevent="setActive(item.name)" :class="[activeComponent === item.name ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold']">
-                          <component :is="item.icon" :class="['w-6 h-6 shrink-0', activeComponent === item.name ? 'text-white' : 'text-indigo-200 group-hover:text-white']" aria-hidden="true" />
-                          {{ item.name }}
-                        </a>
+                          <button @click="setActive(item.name)"
+                            :class="[activeComponent === item.name ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold']">
+                            <component :is="item.icon"
+                              :class="['w-6 h-6 shrink-0', activeComponent === item.name ? 'text-white' : 'text-indigo-200 group-hover:text-white']"
+                              aria-hidden="true" />
+                            {{ item.name }}
+                          </button>
                         </li>
                       </ul>
                     </li>
-                   
+
                     <li class="mt-auto">
-                      <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
-                        <Cog6ToothIcon class="w-6 h-6 shrink-0 text-indigo-200 group-hover:text-white" aria-hidden="true" />
+                      <button
+                        class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white"
+                        @click.prevent="setActive('Settings')" <!-- Assuming you want to handle settings similarly -->
+                        >
+                        <Cog6ToothIcon class="w-6 h-6 shrink-0 text-indigo-200 group-hover:text-white"
+                          aria-hidden="true" />
                         Settings
-                      </a>
+                      </button>
                     </li>
                   </ul>
                 </nav>
@@ -56,23 +70,28 @@
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
         <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=white" alt="Your Company" />
+          <img class="h-8 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=white"
+            alt="Your Company" />
         </div>
         <nav class="flex flex-1 flex-col">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
-                  <a href="#" @click.prevent="setActive(item.name)" :class="[activeComponent === item.name ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold']">
-              <component :is="item.icon" :class="['w-6 h-6 shrink-0', activeComponent === item.name ? 'text-white' : 'text-indigo-200 group-hover:text-white']" aria-hidden="true" />
-              {{ item.name }}
-            </a>
+                  <a href="#" @click.prevent="setActive(item.name)"
+                    :class="[activeComponent === item.name ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold']">
+                    <component :is="item.icon"
+                      :class="['w-6 h-6 shrink-0', activeComponent === item.name ? 'text-white' : 'text-indigo-200 group-hover:text-white']"
+                      aria-hidden="true" />
+                    {{ item.name }}
+                  </a>
                 </li>
               </ul>
             </li>
-            
+
             <li class="mt-auto">
-              <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
+              <a href="#"
+                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
                 <Cog6ToothIcon class="w-6 h-6 shrink-0 text-indigo-200 group-hover:text-white" aria-hidden="true" />
                 Settings
               </a>
@@ -83,7 +102,8 @@
     </div>
 
     <div class="lg:pl-72">
-      <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+      <div
+        class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
         <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
           <Bars3Icon class="w-6 h-6" aria-hidden="true" />
@@ -94,8 +114,11 @@
 
         <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
           <form class="grid flex-1 grid-cols-1" action="#" method="GET">
-            <input type="search" name="search" aria-label="Search" class="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-none placeholder:text-gray-400 sm:text-sm/6" placeholder="Search" />
-            <MagnifyingGlassIcon class="pointer-events-none col-start-1 row-start-1 w-6 h-6 self-center text-gray-400" aria-hidden="true" />
+            <input type="search" name="search" aria-label="Search"
+              class="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-none placeholder:text-gray-400 sm:text-sm/6"
+              placeholder="Search" />
+            <MagnifyingGlassIcon class="pointer-events-none col-start-1 row-start-1 w-6 h-6 self-center text-gray-400"
+              aria-hidden="true" />
           </form>
           <div class="flex items-center gap-x-4 lg:gap-x-6">
             <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
@@ -110,18 +133,26 @@
             <Menu as="div" class="relative">
               <MenuButton class="-m-1.5 flex items-center p-1.5">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-10 h-10 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <img class="w-10 h-10 rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt="" />
                 <span class="hidden lg:flex lg:items-center">
                   <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">{{ val }}</span>
                   <ChevronDownIcon class="ml-2 w-10 h-10 text-gray-400" aria-hidden="true" />
                 </span>
               </MenuButton>
-              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                <!-- <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <transition enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95">
+                <MenuItems
+                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                   <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                    <a @click="profiledashboard(item)"   :class="[ active ? 'bg-gray-50 outline-none cursor-pointer' : '', 'block px-3 py-1 text-sm/6 text-gray-900']">{{ item.name }}</a>
+                  <a @click="profiledashboard(item)"
+                    :class="[active ? 'bg-gray-50 outline-none cursor-pointer' : '', 'block px-3 py-1 text-sm/6 text-gray-900']">{{
+                    item.name }}</a>
                   </MenuItem>
-                </MenuItems> -->
+                </MenuItems>
               </transition>
             </Menu>
           </div>
@@ -129,7 +160,7 @@
       </div>
 
       <main class="py-2">
-       
+
         <div class="px-2">
           <div v-if="activeComponent === 'Ledger'">
             <ledger />
@@ -149,10 +180,10 @@
             <withdraw />
           </div>
           <div class="w-full" v-if="profilepanel">
-  <profile />
-</div>
+            <profile />
+          </div>
 
-   
+
         </div>
       </main>
     </div>
@@ -168,7 +199,7 @@ import trades from '~/components/trades.vue'
 import profiltloss from '~/components/profitloss.vue'
 import funds from '~/components/funds.vue'
 import withdraw from '~/components/withdraw.vue'
-import { onMounted} from "vue";
+import { onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import {
   Dialog,
@@ -195,14 +226,14 @@ import {
 import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
 import Profiltloss from '~/components/profitloss.vue'
 
-const profilepanel=ref(false)
+const profilepanel = ref(false)
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, },
-  { name: 'Ledger', href: '#', icon: UsersIcon,  },
-  { name: 'Trades', href: '#', icon: FolderIcon,  },
-  { name: 'Profit / Loss', href: '#', icon: CalendarIcon,  },
-  { name: 'Funds', href: '#', icon: DocumentDuplicateIcon,  },
-  { name: 'Withdraw', href: '#', icon: ChartPieIcon,  },
+  { name: 'Ledger', href: '#', icon: UsersIcon, },
+  { name: 'Trades', href: '#', icon: FolderIcon, },
+  { name: 'Profit / Loss', href: '#', icon: CalendarIcon, },
+  { name: 'Funds', href: '#', icon: DocumentDuplicateIcon, },
+  { name: 'Withdraw', href: '#', icon: ChartPieIcon, },
 ]
 
 const userNavigation = [
@@ -241,14 +272,14 @@ const profiledashboard = (item) => {
 
 const router = useRouter();
 const route = useRoute();
-const val = ref(route.query.val || 'GWD001'); 
+const val = ref(route.query.val || 'GWD001');
 
 onMounted(() => {
 
   // if (route.query.val) {
-  
+
   //   router.replace({ path: route.path, query: {} });
- 
+
   // }
 });
 
